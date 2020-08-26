28ecfc6095d09ef0379115fe715b4829be2f642f
Damien Diehl <damdiehl@twitch.tv>
2020-08-25 22:16:14 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,21 @@
+// background.js
+// Connect listener to the browserAction button
+// Send message for the content script to analyze the Bingo board
+
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(tab.id, {}).catch(onError);
+  }
+}
+
+browser.browserAction.onClicked.addListener(() => {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(sendMessageToTabs).catch(onError);
+});
+
